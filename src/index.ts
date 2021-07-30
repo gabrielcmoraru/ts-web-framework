3ea5497d3b861c533eb33534e2df929792e283aa
Gabriel Moraru <gabrielcmoraru@github.com>
2021-07-30 19:18:44 +0100

    add user model and basic event handler

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('test');
+import { User } from './models/Users';
+
+const user = new User({ name: 'bobs', age: 31 });
+
+user.on('change', () => {
+  console.log('ch 1');
+});
+user.on('change', () => {
+  console.log('ch 2');
+});
+user.on('asd', () => {
+  console.log('asd');
+});
+
+user.trigger('change');
+user.trigger('asd');
+
+user.set({ name: 'Adri' });
+console.log(user.get('name'));
+console.log(user.get('age'));
+console.log(user);
