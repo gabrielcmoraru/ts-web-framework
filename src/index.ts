136d66ecf9582d37b55d3ef02d290597117d82a1
Gabriel Moraru <gabrielcmoraru@github.com>
2021-07-31 12:09:00 +0100

    add html render clas

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import { User } from './models/Users';
+import { UserForm } from './views/UserForm';
 
-const collection = User.buildUserCollection();
-collection.on('change', () => {
-  return console.log('change', collection);
-});
-collection.fetch();
+const user = User.buildUser({ age: 333, name: 'NAME' });
+const root = document.getElementById('root');
 
-// const user = User.buildUser({ id: 1, name: 'bobs' });
+if (root) {
+  const userForm = new UserForm(root, user);
+
+  userForm.render();
+} else {
+  throw new Error('Root element not found');
+}
+
+// const collection = User.buildUserCollection();
+// collection.on('change', () => {
+//   return console.log('change', collection);
+// });
+// collection.fetch();
+
 // user.on('change', () => {
 //   console.log('change', user);
 // });
