673cbfa35acceaff204d9cd5e4c42207eaaf0b39
Gabriel Moraru <gabrielcmoraru@github.com>
2021-07-31 09:53:26 +0100

    add accessors to user class, fix this scoping on methods

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import { User } from './models/Users';
 
 const user = new User({ id: 2 });
+console.log(user.get('id'));
+user.on('change', () => {
+  console.log('change triggered'))
+user.trigger('change')
